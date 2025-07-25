b52efacf5240598727398a8d152a7e8af5ddf03e
Duc Pham <ducpm0511@gmail.com>
2025-07-25 16:20:17 +0700

    requrire ssl in connection to database

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
   synchronize: process.env.TYPEORM_SYNC === 'true',
   logging: process.env.TYPEORM_LOGGING === 'true',
   entities: [process.env.NODE_ENV === 'production' ? 'dist/entities/**/*.js' : 'src/entities/**/*.ts'],
