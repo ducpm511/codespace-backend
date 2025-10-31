1f1d860656e59d7932fee9ac090e4a33816baa61
Duc Pham <ducpm0511@gmail.com>
2025-10-31 15:19:32 +0700

    - fixed missing shiftentity

--- src/staff-schedules/staff-schedules.module.ts
+++ src/staff-schedules/staff-schedules.module.ts
@@ -4,10 +4,15 @@ import { StaffScheduleEntity } from '../entities/staff-schedule.entity';
 import { StaffSchedulesController } from './staff-schedules.controller';
 import { StaffSchedulesService } from './staff-schedules.service';
 import { ClassSessionEntity } from '../entities/class-session.entity';
+import { ShiftEntity } from 'src/entities/shift.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([StaffScheduleEntity, ClassSessionEntity]),
+    TypeOrmModule.forFeature([
+      StaffScheduleEntity,
+      ClassSessionEntity,
+      ShiftEntity,
+    ]),
   ],
   controllers: [StaffSchedulesController],
   providers: [StaffSchedulesService],
